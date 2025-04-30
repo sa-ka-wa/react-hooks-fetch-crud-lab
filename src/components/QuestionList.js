e1c7c2560ebefeb0787f3c365a860d0e1bd1e790
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-04-30 11:39:12 +0300

    Implement question submission and deletion functionality

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then((res) => {
+      if (res.ok) {
+        setQuestions(questions.filter((q) => q.id !== id));
+      }
+    });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDelete={handleDelete}
+            // Add props for delete/update later
+          />
+        ))}
+      </ul>
     </section>
   );
 }
